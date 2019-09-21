2a089f932d77b448cf681346e7a31b4bca9e636a
zizu9 <gudaz90@gmail.com>
2019-09-20 20:36:20 -0500

    Use Maps to store todoCollection
    also add removeComplete method
    also add geTodos method

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
 
 let todos: TodoItem[] = [
-    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
-    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)
+    new TodoItem(1, "Buy Flowers"),
+    new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"),
+    new TodoItem(4, "Call Joe", true)
 ];
 
 let collection: TodoCollection = new TodoCollection("Adam", todos);
@@ -11,8 +13,7 @@ let collection: TodoCollection = new TodoCollection("Adam", todos);
 console.clear();
 console.log(`${collection.userName}'s Todo List`);
 
-let newId: number = collection.addTodo("Go for run");
-let todoItem: TodoItem = collection.getTodoById(newId);
-todoItem.printDetails();
+collection.removeComplete();
+collection.getTodoItems(true).forEach(item => item.printDetails());
 
 // collection.addTodo(todoItem);
