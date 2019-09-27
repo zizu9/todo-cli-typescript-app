b528881f0cc903c2080250e631ae3665bebc7ee0
zizu9 <gudaz90@gmail.com>
2019-09-27 01:26:28 -0500

    Add Task, Mark as Complete and purge tasks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from 'inquirer';
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"),
@@ -9,11 +10,82 @@ let todos: TodoItem[] = [
 ];
 
 let collection: TodoCollection = new TodoCollection("Adam", todos);
+let showCompleted = true;
 
-console.clear();
-console.log(`${collection.userName}'s Todo List`);
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${ collection.getItemCounts().incomplete } items to do)`);
+    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
+}
 
-collection.removeComplete();
-collection.getTodoItems(true).forEach(item => item.printDetails());
+enum Commands {
+    Add = "Add New Task",
+    Complete = "Complete Task",
+    Toggle = "Show/Hide Completed",
+    Purge = "Remove Completed Tasks",
+    Quit = "Quit"
+}
 
-// collection.addTodo(todoItem);
+function promptAdd(): void {
+    console.clear();
+    inquirer.prompt({ type: "input", name: "add", message: "Enter task:"})
+        .then(answers => {
+            if (answers["add"] !== "") {
+                collection.addTodo(answers["add"]);
+            }
+            promptUser();
+        });
+}
+
+function promptComplete(): void {
+    console.clear();
+    inquirer.prompt({
+        type: "checkbox",
+        name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map(item => (
+            { name: item.task, value: item.id, checked: item.complete }
+        ))
+    }).then(answers => {
+        let completedTasks = answers["complete"] as number[];
+        collection.getTodoItems(true).forEach(item => (
+            collection.markComplete(item.id,
+                completedTasks.find(id => id === item.id) != undefined)
+        ));
+        promptUser();
+    })
+}
+
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                } else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break;
+        }
+    })
+}
+
+promptUser();
--- src/todoCollection.ts
+++ src/todoCollection.ts
@@ -1,5 +1,10 @@
 import { TodoItem } from "./todoItem";
 
+type ItemCounts = {
+    total: number,
+    incomplete: number
+}
+
 export class TodoCollection {
     private nextId: number = 1;
     private itemMap = new Map<number, TodoItem>();
@@ -41,6 +46,13 @@ export class TodoCollection {
                 this.itemMap.delete(item.id);
             }
         })
+    }
+
+    getItemCounts(): ItemCounts {
+        return {
+            total: this.itemMap.size,
+            incomplete: this.getTodoItems(false).length
+        };
     }
 
 }
